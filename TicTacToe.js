c146f58fe3162f9b71fa74d91cb8b32b28eddec4
Kondorcom <kondorcom88@gmail.com>
2022-10-12 05:58:43 +0200

    Add X to board on press

--- TicTacToe.js
+++ TicTacToe.js
@@ -3,14 +3,52 @@ var gameboard = (function() {
     'use strict';
     const table = document.getElementById("gameBoardTable");
     var td1 = document.getElementById('1');
+    // td1.onclick = function(){
+        
+    //     td1.innerHTML = "X";
+    // };
+    // var cells = document.querySelectorAll(1, '2','3','4','5','6','7','8','0');
+    // function drawXO(){
+    //     cells.onclick = function(){
+    //         console.log('drawXO');
+    //         cells.innerHTML = 'x';
+    //     };
+    // }
+    
+    function drawXO(){
+        for (let i=0; i<9; i++){
+           
+              document.getElementById(i).onclick = function(){
+                let selectedCell = document.getElementById(i);
+                console.log(selectedCell.textContent);
+                if (selectedCell.textContent === ' '){    
+                    selectedCell.textContent = 'X';
+                    console.log('should write x');
+                } else {
+                    selectedCell.textContent = ' ';
+                    console.log('x should change to empty');
+                }
+                
+                // console.log(currentCell);
+                console.log('cell pos',i);
+                // td1.textContent = "X";
+                // let txt = document.createTextNode('xx');
+                // td1.appendChild(txt);
+                // currentCell.appendChild(txt);
+             };
+        }
+        
+    }
 
-    td1.innerHTML = "X";
+    // td1.innerHTML = "X";
+
     var _boardArray = [];
 
     function setBoard(){
         for (var i=0; i<9; i++){
             _boardArray[i] = 0;
             document.getElementById(i).bgColor = '#2E8B57';
+            document.getElementById(i).innerText = ' ';
 
         }
     
@@ -48,11 +86,13 @@ var gameboard = (function() {
         playerOneMove:playerTwoMove,
       setBoard: setBoard,
       getBoardPosStatus: getBoardPosStatus,
-      displayAllPositions: displayAllPositions
+      displayAllPositions: displayAllPositions,
+      drawXO:drawXO
     };
   })();
-
   gameboard.setBoard();
+  gameboard.drawXO();
+ 
   gameboard.displayAllPositions();
   gameboard.getBoardPosStatus(3);
 
