6f96bd4a38ba8a4ee83faf6ee988d2718c9d76cc
Kondorcom <kondorcom88@gmail.com>
2022-10-14 05:24:11 +0200

    Add score

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,47 +2,97 @@
 var gameboard = (function() {
     'use strict';
     const table = document.getElementById("gameBoardTable");
-    var td1 = document.getElementById('1');
-    // td1.onclick = function(){
-        
-    //     td1.innerHTML = "X";
-    // };
-    // var cells = document.querySelectorAll(1, '2','3','4','5','6','7','8','0');
-    // function drawXO(){
-    //     cells.onclick = function(){
-    //         console.log('drawXO');
-    //         cells.innerHTML = 'x';
-    //     };
-    // }
-    
-    function drawXO(){
+    var _boardArray = [];
+
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+
+    let plOneScore = document.getElementById('plOneScore');
+    let plTwoScore = document.getElementById('plTwoScore');
+
+    plOneScore.textContent = playerOneScore;
+    plTwoScore.textContent = playerTwoScore;
+
+    let plOneArea = document.getElementById('plOne');
+    let plTwoArea = document.getElementById('plTwo');
+
+    const plOne = 'X';
+    const plTwo = 'O';
+    const draw = 'D';
+
+    let currentPlayer;
+
+    let winnerOneOrTwo = 'I';
+
+    function drawXO(){  
+
+        currentPlayer = plOne;
+        plOneArea.style.backgroundColor = 'lightblue';
+
         for (let i=0; i<9; i++){
            
               document.getElementById(i).onclick = function(){
                 let selectedCell = document.getElementById(i);
-                console.log(selectedCell.textContent);
+                // console.log(selectedCell.textContent);
                 if (selectedCell.textContent === ' '){    
-                    selectedCell.textContent = 'X';
-                    console.log('should write x');
+                    selectedCell.textContent = currentPlayer;
+                    _boardArray[i] = currentPlayer;
+                    // console.log('should write x');
                 } else {
-                    selectedCell.textContent = ' ';
+                    // selectedCell.textContent = ' ';
+                    alert('already played');
                     console.log('x should change to empty');
+                }   
+                if (currentPlayer === plOne){
+                    plOneArea.style.backgroundColor = 'white';
+                    plTwoArea.style.backgroundColor = 'lightblue';
+                    currentPlayer = plTwo;
+                }   else {
+                    plTwoArea.style.backgroundColor = 'white';
+                    plOneArea.style.backgroundColor = 'lightblue';
+                    currentPlayer = plOne;
                 }
+                // console.log('position on board', i); 
+
+                checkIfWin();
+                if (winnerOneOrTwo === plOne){
+                    playerOneScore ++;
+                    plOneScore.textContent = playerOneScore;
+                    alert('would you like to play again');
+                    setBoard();
+                }   else if (winnerOneOrTwo === plTwo){
+                    playerTwoScore ++;
+                    plTwoScore.textContent = playerTwoScore;
+                    alert('would you like to play again');
+                    setBoard();
+                }   else if(winnerOneOrTwo === draw){
+                    alert('DRAW, would you like to play again');
+                    setBoard();
+                }
+
+                // if (winnerOneOrTwo === plOne || winnerOneOrTwo === plTwo ){
+                    
+                //     alert('would you like to play again');
+                //     setBoard();
+                // }   else if (winnerOneOrTwo === draw) {
+                //     alert('DRAW, would you like to play again');
+                //     setBoard();
+                // }
                 
-                // console.log(currentCell);
-                console.log('cell pos',i);
-                // td1.textContent = "X";
-                // let txt = document.createTextNode('xx');
-                // td1.appendChild(txt);
-                // currentCell.appendChild(txt);
              };
         }
-        
+     
+    }  
+   
+    
+    function play(){
+        let showPlayerOne = document.getElementById('plOne');
+        let showPlayerTwo = document.getElementById('plTwo');
+
+        showPlayerOne.style.backgroundColor = 'lightblue';
+
+
     }
-
-    // td1.innerHTML = "X";
-
-    var _boardArray = [];
 
     function setBoard(){
         for (var i=0; i<9; i++){
@@ -50,25 +100,41 @@ var gameboard = (function() {
             document.getElementById(i).bgColor = '#2E8B57';
             document.getElementById(i).innerText = ' ';
 
-        }
-    
-        
-        // document.getElementById('0').bgColor = '#00FF00';
-      
-  
-
-        // td1.textContent= "X";
-        
-        
+        }      
+        winnerOneOrTwo = 'unknown';  
+        drawXO();
+       
     }
     function getBoardPosStatus(x){
         console.log(_boardArray[8]);
     }
     function displayAllPositions(){
         for (var i=0;i< 9;i++){
-            console.log(_boardArray[i]);
+            console.log(i,'===',_boardArray[i]);
+            
         }
     }
+    function checkAllPositions(){
+        let check = 0;
+        // console.log('checkPos');
+        for (var i=0;i< 9;i++){
+
+            // console.log(i,'===',_boardArray[i]);
+            if (_boardArray[i] == ' '){
+                
+                check ++;
+            }  
+        }
+        // console.log(check);
+        if (check === 0){
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+    let checking = checkAllPositions();
+
    function playerOneMove(x){
         _boardArray[x] = 'X';
    }
@@ -80,27 +146,86 @@ var gameboard = (function() {
             console.log('position already played, choose again');
         }
     }
+   function checkIfWin(){
+    // console.log('checking win pos');
+        let checking = checkAllPositions();
+        if (' ' != _boardArray[0] && _boardArray[0]===_boardArray[1] && _boardArray[1] ===_boardArray[2]){
+            if (_boardArray[0] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }   else if (' ' != _boardArray[3] && _boardArray[3]===_boardArray[4] && _boardArray[4] ===_boardArray[5]){
+            if (_boardArray[3] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }   else if (' ' != _boardArray[6] && _boardArray[6]===_boardArray[7] && _boardArray[7] ===_boardArray[8]){
+            if (_boardArray[6] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }   else if (' ' != _boardArray[0] && _boardArray[0]===_boardArray[3] && _boardArray[3] ===_boardArray[6]){
+            if (_boardArray[0] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }   else if (' ' != _boardArray[1] && _boardArray[1]===_boardArray[4] && _boardArray[4] ===_boardArray[7]){
+            if (_boardArray[1] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }   else if (' ' != _boardArray[2] && _boardArray[2]===_boardArray[5] && _boardArray[5] ===_boardArray[8]){
+            if (_boardArray[2] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }   else if (' ' != _boardArray[0] && _boardArray[0]===_boardArray[4] && _boardArray[4] ===_boardArray[8]){
+            if (_boardArray[0] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }   else if (' ' != _boardArray[2] && _boardArray[2]===_boardArray[4] && _boardArray[4] ===_boardArray[6]){
+            if (_boardArray[2] === plOne){
+                winnerOneOrTwo = plOne;
+            }   else {winnerOneOrTwo = plTwo}
+            console.log('winner is ' + winnerOneOrTwo);
+        }  else if(checking === 1){
+            console.log('checkAllPos');
+             winnerOneOrTwo = draw;
+
+        }
+        
+        // console.log(checking);
+        
+    }
   
     return {
-        playerOneMove:playerOneMove,
-        playerOneMove:playerTwoMove,
-      setBoard: setBoard,
-      getBoardPosStatus: getBoardPosStatus,
-      displayAllPositions: displayAllPositions,
-      drawXO:drawXO
+      
+        setBoard: setBoard,
+        
     };
   })();
   gameboard.setBoard();
-  gameboard.drawXO();
+//   gameboard.drawXO();
  
-  gameboard.displayAllPositions();
-  gameboard.getBoardPosStatus(3);
+ 
+const Player = (name) => {
+        // let health = level * 2;
+        // const getLevel = () => level;
+        const getName  = () => name;
+        let playedPositions = [];
+        const gamesWon = 0;
+
+        const playedPosition = x =>{
+            playedPosition[x] = 1;
+        }
+
+        
+        return {attack, damage, getLevel, getName};
+      };
 
 
-//   myModule.publicMethod(); // outputs 'Hello World'
-//   console.log(myModule.publicProperty); // outputs 'I am a public property'
-//   console.log(myModule._privateProperty); // is undefined protected by the module closure
-//   myModule._privateMethod(); // is TypeError protected by the module closure
+
 
 //   const Player = (name) => {
 //     // let health = level * 2;
